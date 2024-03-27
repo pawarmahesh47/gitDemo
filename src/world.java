cc345f4318f5cc764e5a4896272e15c368d10d3a
mahesh <pawar.mahesh47@gmail.com>
2024-03-27 12:14:19 +0530

    This is commit from Asian guy

--- src/world.java
+++ src/world.java
@@ -5,6 +5,9 @@ public class world
 	public static void main(String[] args) {
 		
 		System.out.print("hello world");
+		System.out.print("hello world 1");
+		System.out.print("hello world 2");
+		System.out.print("hello world 3");
 
 	}
 	
